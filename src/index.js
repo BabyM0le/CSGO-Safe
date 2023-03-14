da43b183cd72775ed4f3e8d4c6fea632974b5068
TomPadmanathan <padmanathantom@gmail.com>
2023-03-14 18:26:12 +0000

    Updated import in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-import { CsgoRepInfo } from './components/CsgoRepInfo.js';
+import { CsgoRepInfo } from './components/CsgoRep/RequestHtml.js';
 const steamId = '76561199095221628';
 console.log(await CsgoRepInfo(steamId));
