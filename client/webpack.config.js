6e208642fa56c0384d4afa31807765793dc55c5e
Tom Padmanathan <padmanathantom@gmail.com>
2023-03-20 02:59:22 +0000

    Updated package.json & webpack.config.js to use jsx

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,7 +4,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.js',
+    entry: './src/index.jsx',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
@@ -12,7 +12,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
@@ -50,4 +50,7 @@ module.exports = {
         }),
         new CleanWebpackPlugin(),
     ],
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
 };
