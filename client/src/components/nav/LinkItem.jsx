98a1b0d141b46058a61adea71cba402273700742
Tom Padmanathan <padmanathantom@gmail.com>
2023-03-24 02:17:15 +0000

    Improved spacing on nav

--- client/src/components/nav/LinkItem.jsx
+++ client/src/components/nav/LinkItem.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function LinkItem(props) {
     return (
-        <li className="p-3 m-5 flex rounded hover:bg-blue-600 transition-all cursor-pointer">
+        <li className="p-3 my-5 mx-2 flex rounded hover:bg-blue-600 transition-all cursor-pointer">
             <img className="h-8 w-8 mr-2" src={props.icon}></img>
             <p className="text-2xl">{props.name}</p>
         </li>
--- client/src/components/nav/Navbar.jsx
+++ client/src/components/nav/Navbar.jsx
@@ -10,11 +10,11 @@ import HomeIcon from '../../assets/img/home-icon.png';
 export default function Navbar() {
     return (
         <nav className="bg-gray-700 text-white rounded-2xl flex justify-between mt-5 mx-10">
-            <a href="/" className="flex p-3 m-5">
+            <a href="/" className="flex p-3 my-5 ml-8">
                 <h1 className="text-2xl">CSGO Safe</h1>
                 <img src={HomeIcon} className="w-8 h-8 ml-2"></img>
             </a>
-            <ul className="flex space-around justify-around">
+            <ul className="flex justify-around mr-8">
                 <LinkItem icon={HomeIcon} name={'Check Reputation'} />
                 <LinkItem name={'Source Code'} />
                 <LinkItem name={'Discord Bot'} />
