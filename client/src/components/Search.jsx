01b52009efe4b20b5261bd9ca9d77391fe5dab55
TomPadmanathan <padmanathantom@gmail.com>
2023-03-30 22:06:40 +0100

    Added express to backend

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -5,7 +5,7 @@ export default function Search() {
     return (
         <form
             className="h-screen flex items-center justify-center"
-            action="POST"
+            action="http://localhost:3001/api"
         >
             <input
                 className="border-grey-900 border-b-4 text-black focas:border-black focas:cursor-white outline-none mr-4 h-24 w-2/3 pl-5 text-4xl bg-blue-700"
--- server/src/index.js
+++ server/src/index.js
@@ -1,24 +1,39 @@
-// CSGOREP
-import { RequestCsgoRepHTML } from './components/CsgoRep/RequestData.js';
-import { HandleCsgoRepHTML } from './components/CsgoRep/HandleData.js';
+// // CSGOREP
+// import { RequestCsgoRepHTML } from './components/CsgoRep/RequestData.js';
+// import { HandleCsgoRepHTML } from './components/CsgoRep/HandleData.js';
 
-// STEAMREP
-import { RequestSteamRepHTML } from './components/SteamRep/RequestData.js';
+// // STEAMREP
+// import { RequestSteamRepHTML } from './components/SteamRep/RequestData.js';
 
-const steamId = '76561199095221628';
-const testing = async () => {
-    // const CsgoRepHTML = await RequestCsgoRepHTML(steamId);
-    // const HandledData = new HandleCsgoRepHTML();
-    // console.log(HandledData.GetTotalDeals(CsgoRepHTML));
-    // console.log(HandledData.GetPositiveDeals(CsgoRepHTML));
-    // console.log(HandledData.GetNeutralDeals(CsgoRepHTML));
-    // console.log(HandledData.GetBannedStatus(CsgoRepHTML));
-    // console.log(HandledData.GetCashDeals(CsgoRepHTML));
-    // console.log(HandledData.GetCryptoDeals(CsgoRepHTML));
-    // console.log(HandledData.GetBalanceDeals(CsgoRepHTML));
-    // console.log(HandledData.GetAverageRepsOfDeals(CsgoRepHTML));
-    // console.log(HandledData.GetAmountOfGoingFirst(CsgoRepHTML));
-    // console.log(HandledData.GetAmountOfGoingSecond(CsgoRepHTML));
-    console.log(await RequestSteamRepHTML(steamId));
-};
-testing();
+// const steamId = '76561199095221628';
+// const testing = async () => {
+//     // const CsgoRepHTML = await RequestCsgoRepHTML(steamId);
+//     // const HandledData = new HandleCsgoRepHTML();
+//     // console.log(HandledData.GetTotalDeals(CsgoRepHTML));
+//     // console.log(HandledData.GetPositiveDeals(CsgoRepHTML));
+//     // console.log(HandledData.GetNeutralDeals(CsgoRepHTML));
+//     // console.log(HandledData.GetBannedStatus(CsgoRepHTML));
+//     // console.log(HandledData.GetCashDeals(CsgoRepHTML));
+//     // console.log(HandledData.GetCryptoDeals(CsgoRepHTML));
+//     // console.log(HandledData.GetBalanceDeals(CsgoRepHTML));
+//     // console.log(HandledData.GetAverageRepsOfDeals(CsgoRepHTML));
+//     // console.log(HandledData.GetAmountOfGoingFirst(CsgoRepHTML));
+//     // console.log(HandledData.GetAmountOfGoingSecond(CsgoRepHTML));
+//     console.log(await RequestSteamRepHTML(steamId));
+// };
+// testing();
+
+import express from 'express';
+import bodyParser from 'body-parser';
+const app = express();
+const port = 3001;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('http://localhost:3001/api', (req, res) => {
+    res.send('<h1>Thank you for contacting</h1>');
+});
+
+app.listen(port, () => {
+    console.log(`Node running on port: ${port}`);
+});
